3b555c46d8de23a426baa5cfe9722b9ac0b56ffe
Andrey Litvinov <litwinoff@gmail.com>
2016-11-21 19:47:57 +0300

    HelloWorld Servlet (Maven, Tomcat)

---
+++ task-01/src/main/java/HelloWorld.java
@@ -0,0 +1,52 @@
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    private String responseTemplate =
+            "<html>\n" +
+                    "<body>\n" +
+                    	"<h2>Hello from Simple Servlet</h2>\n" +
+                    "</body>\n" +
+            "</html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.process(request, response);
+	}
+	
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(200);
+        response.getWriter().write(responseTemplate);
+    }
+
+}
