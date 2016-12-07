f53a2c88e464c62c36e6dfffdb3db899688070d0
Andrey Litvinov <litwinoff@gmail.com>
2016-12-07 16:56:22 +0300

    Lesson03: MessageSource

--- lesson-02/src/main/java/HelloWorld.java
+++ lesson-02/src/main/java/HelloWorld.java
@@ -2,12 +2,14 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -15,40 +17,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 
 public class HelloWorld extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloWorld() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
+	private ApplicationContext ctx;
 
+    public void init(ServletConfig config) throws ServletException {
+    	this.ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
+    }
+        
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		this.process(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		this.process(request, response);
 	}
 	
     private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+    	response.setContentType("text/html;charset=UTF-8");    	
+		LocaleContextHolder.setLocale(request.getLocale());
+    	    	
     	response.setStatus(200);
         PrintWriter writer = response.getWriter();
-        
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");        
-        MessageRenderer mr = ctx.getBean("renderer",MessageRenderer.class);        
-                
+                        
+        MessageRenderer mr = ctx.getBean("renderer",MessageRenderer.class);
         mr.printMessage(writer);
                 
     }
--- lesson-02/src/main/java/WriterMessageRenderer.java
+++ lesson-02/src/main/java/WriterMessageRenderer.java
@@ -1,14 +1,17 @@
 import java.io.PrintWriter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.context.i18n.LocaleContextHolder;
  
-public class WriterMessageRenderer implements MessageRenderer {
+public class WriterMessageRenderer implements MessageRenderer, MessageSourceAware {
+		
+	private MessageSource  messageSource;
 	
-	private String message;
-	
-	public void setMessage(String message) {
-		this.message = message;		
+	public void setMessageSource(MessageSource  messageSource) {
+		this.messageSource = messageSource;		
 	}
 
     public void printMessage(PrintWriter writer) {
-    	writer.println(message);
+    	writer.println(messageSource.getMessage("msg", null, LocaleContextHolder.getLocale()));
     }
 }
